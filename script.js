1a5745f06b837426d4853b00289ef84d363b49d0
akalampv <dev.akalampv@gmail.com>
2021-10-17 21:15:31 +0300

    Letter press event handler

--- script.js
+++ script.js
@@ -29,4 +29,43 @@ function displayWord() {
     }
 }
 
+// Update the wrong letters element
+function updateWrongLettersEl() {
+    console.log("Update wrong");
+}
+
+// Show notification
+function showNotification() {
+     notification.classList.add("show");
+
+     setTimeout(() => {
+         notification.classList.remove("show");
+     }, 1500);
+}
+
+// Keydown letter press
+window.addEventListener("keydown", e => {
+   if (e.keyCode >= 65 && e.keyCode <= 90) {
+       const letter = e.key;
+
+       if (selectedWord.includes(letter)) {
+           if (!correctLetters.includes(letter)) {
+               correctLetters.push(letter);
+
+               displayWord();
+           } else {
+               showNotification();
+           }
+       } else {
+           if (!wrongLetters.includes(letter)) {
+               wrongLetters.push(letter);
+
+               updateWrongLettersEl();
+           } else {
+               showNotification();
+           }
+       }
+   }
+});
+
 displayWord();
